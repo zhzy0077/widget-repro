ca99d02f56475915c4816799f1e8dff97f42ee78
Zhiyuan Zheng (from Dev Box) <zhizhen@microsoft.com>
2024-05-09 15:25:00 +0800

    add repro.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿
+using System.Runtime.InteropServices;
+using ComTypes = System.Runtime.InteropServices.ComTypes;
+using Microsoft.Windows.Widgets;
+using COM;
+using System;
+using ConsoleApp1;
+using Microsoft.UI.Xaml.Controls;
+
+[DllImport("kernel32.dll")]
+static extern IntPtr GetConsoleWindow();
+
+[DllImport("ole32.dll")]
+
+static extern int CoRegisterClassObject(
+            [MarshalAs(UnmanagedType.LPStruct)] Guid rclsid,
+            [MarshalAs(UnmanagedType.IUnknown)] object pUnk,
+            uint dwClsContext,
+            uint flags,
+            out uint lpdwRegister);
+
+[DllImport("ole32.dll")] static extern int CoRevokeClassObject(uint dwRegister);
+
+Console.WriteLine("Registering Widget Provider");
+uint cookie;
+
+
+
+Guid CLSID_Factory = Guid.Parse("97E8E77F-0E70-4B67-BF98-03E21262396F");
+CoRegisterClassObject(CLSID_Factory, new WidgetProviderFactory<WidgetProvider>(), 0x4, 0x1, out cookie);
+Console.WriteLine("Registered successfully. Press ENTER to exit.");
+Console.ReadLine();
+
+if (GetConsoleWindow() != IntPtr.Zero)
+{
+    Console.WriteLine("Registered successfully. Press ENTER to exit.");
+    Console.ReadLine();
+}
+else
+{
+    // Wait until the manager has disposed of the last widget provider.
+    using (var emptyWidgetListEvent = WidgetProvider.GetEmptyWidgetListEvent())
+    {
+        emptyWidgetListEvent.WaitOne();
+    }
+
+    CoRevokeClassObject(cookie);
+}
---
+++ ConsoleApp1/WidgetProvider.cs
@@ -0,0 +1,166 @@
+﻿using Microsoft.Windows.Widgets.Providers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+
+    internal class WidgetProvider : IWidgetProvider, IWidgetProvider2
+    {
+        static ManualResetEvent emptyWidgetListEvent = new ManualResetEvent(false);
+
+        public static ManualResetEvent GetEmptyWidgetListEvent()
+        {
+            return emptyWidgetListEvent;
+        }
+
+        public static Dictionary<string, WidgetInfo> RunningWidgets = new Dictionary<string, WidgetInfo>();
+
+        const string countWidgetTemplate = """
+{                                                                     
+    "type": "AdaptiveCard",                                         
+    "body": [                                                         
+        {                                                               
+            "type": "TextBlock",                                    
+            "text": "You are in normal mode."    
+        }                                                            
+    ],                                                                  
+    "actions": [                                                            
+    ],                                                                  
+    "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+    "version": "1.5"                                                
+}
+""";
+        const string customizationWidgetTemplate = """
+{                                                                     
+    "type": "AdaptiveCard",                                         
+    "body": [                                                         
+        {                                                               
+            "type": "TextBlock",                                    
+            "text": "You are in edit mode."    
+        }                                                            
+    ],                                                                  
+    "actions": [                                                      
+        {                                                               
+            "type": "Action.Execute",                               
+            "title": "Exit",                                   
+            "verb": "exit"                                           
+        }                                                               
+    ],                                                                  
+    "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+    "version": "1.5"                                                
+}
+""";
+
+        public void CreateWidget(WidgetContext widgetContext)
+        {
+            var widgetId = widgetContext.Id; // To save RPC calls
+            var widgetName = widgetContext.DefinitionId;
+            var runningWidgetInfo = new WidgetInfo() { widgetId = widgetId, customizationMode = false };
+            RunningWidgets[widgetId] = runningWidgetInfo;
+
+
+            // Update the widget
+            UpdateWidget(runningWidgetInfo);
+        }
+
+        public void DeleteWidget(string widgetId, string customState)
+        {
+            RunningWidgets.Remove(widgetId);
+            if (RunningWidgets.Count == 0)
+            {
+                emptyWidgetListEvent.Set();
+            }
+        }
+        public void OnCustomizationRequested(WidgetCustomizationRequestedArgs customizationInvokedArgs)
+        {
+            var widgetId = customizationInvokedArgs.WidgetContext.Id;
+            if (RunningWidgets.ContainsKey(widgetId))
+            {
+                var localWidgetInfo = RunningWidgets[widgetId];
+                localWidgetInfo.customizationMode = true;
+                UpdateWidget(localWidgetInfo);
+            }
+        }
+        public void OnActionInvoked(WidgetActionInvokedArgs actionInvokedArgs)
+        {
+            var verb = actionInvokedArgs.Verb;
+            if (verb == "exit")
+            {
+                var widgetId = actionInvokedArgs.WidgetContext.Id;
+                // If you need to use some data that was passed in after
+                // Action was invoked, you can get it from the args:
+                var data = actionInvokedArgs.Data;
+                if (RunningWidgets.ContainsKey(widgetId))
+                {
+                    var localWidgetInfo = RunningWidgets[widgetId];
+                    // Increment the count
+                    localWidgetInfo.customizationMode = false;
+                    UpdateWidget(localWidgetInfo);
+                }
+            }
+        }
+        public void OnWidgetContextChanged(WidgetContextChangedArgs contextChangedArgs)
+        {
+            var widgetContext = contextChangedArgs.WidgetContext;
+            var widgetId = widgetContext.Id;
+            var widgetSize = widgetContext.Size;
+            if (RunningWidgets.ContainsKey(widgetId))
+            {
+                var localWidgetInfo = RunningWidgets[widgetId];
+                UpdateWidget(localWidgetInfo);
+            }
+        }
+        public void Activate(WidgetContext widgetContext)
+        {
+            var widgetId = widgetContext.Id;
+
+            if (RunningWidgets.ContainsKey(widgetId))
+            {
+                var localWidgetInfo = RunningWidgets[widgetId];
+
+                UpdateWidget(localWidgetInfo);
+            }
+        }
+        public void Deactivate(string widgetId)
+        {
+            if (RunningWidgets.ContainsKey(widgetId))
+            {
+                var localWidgetInfo = RunningWidgets[widgetId];
+                UpdateWidget(localWidgetInfo);
+            }
+        }
+
+        void UpdateWidget(WidgetInfo localWidgetInfo)
+        {
+            WidgetUpdateRequestOptions updateOptions = new WidgetUpdateRequestOptions(localWidgetInfo.widgetId);
+
+            string? templateJson = null;
+
+            if (localWidgetInfo.customizationMode)
+            {
+                templateJson = customizationWidgetTemplate;
+            }
+            else
+            {
+                templateJson = countWidgetTemplate;
+            }
+
+            string? dataJson = "{}";
+
+            updateOptions.Template = templateJson;
+            updateOptions.Data = dataJson;
+            WidgetManager.GetDefault().UpdateWidget(updateOptions);
+        }
+    }
+
+    internal class WidgetInfo
+    {
+        public string? widgetId { get; set; }
+        public bool customizationMode { get; set; }
+    }
+}
